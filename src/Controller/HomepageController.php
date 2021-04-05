7ed30b3d1bae48e5b8bc602dd20d21cbc6c4df32
Marine <lancelinmarine@gmail.com>
2021-04-05 17:24:09 +0200

    Fin de l'intégration de la page présentation

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -10,13 +10,17 @@ use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\String\Slugger\SluggerInterface;
 
+use App\Entity\Contact;
+use App\Form\ContactType;
+
 class HomepageController extends AbstractController
 {
     /**
      * @Route("/", name="homepage")
      * Page d'accueil du site internet
      */
-    public function index(): Response
+    // public function index(Request $request, \Swift_Mailer $mailer): Response
+    public function index(Request $request): Response
     {
         /**
          * CONFIG
@@ -27,6 +31,60 @@ class HomepageController extends AbstractController
         /**
          * ELEMENTS
          */
+            // GET Form
+            $form = $this->createForm(ContactType::class, new Contact);
+        //
+
+        /**
+         * TRAITEMENT
+         */
+            $form->handleRequest($request);
+            if ($request->isMethod('POST')) {
+        
+                if ($form->isSubmitted() && $form->isValid()) {
+                    $contact = $form->getData();
+                    $contact->setDate(new \DateTime(date('Y-m-d H:i:s')));
+                    $em->persist($contact);
+                    $em->flush();
+
+
+                    /***** ENVOI DU MAIL *****/
+
+                        // On initialise le transport
+                        $transport = new \Swift_SmtpTransport( $this->getParameter('MAILER_SMTP'), $this->getParameter('MAILER_PORT'), $this->getParameter('MAILER_ENCRYPTION') );
+                        $transport->setUsername( $this->getParameter('MAILER_EMAIL') )->setPassword( $this->getParameter('MAILER_PASSWORD') );
+
+                        // On initialise le mailer
+                        $mailer = new \Swift_Mailer( $transport );
+
+                        // On initialise le message
+                        $message = new \Swift_Message();
+
+                        // On crée le mail
+                        $message->setSubject( 'Site - Demande de contact' )
+                                ->setFrom( $this->getParameter('MAILER_EMAIL') )
+                                ->setTo( 'lancelinmarine@gmail.com' )
+                                ->setBody( '<p>Bonjour,</p><p>Vous avez une nouvelle demande de contact :</p>'.
+                                            '<ul>'.
+                                            '<li>Nom : '.$contact->getPrenom().' '.$contact->getNom().'</li>'.
+                                            '<li>Téléphone : '.$contact->getTelephone().'</li>'.
+                                            '<li>E-mail : '.$contact->getEmail().'</li>'.
+                                            '<li>Message : '.$contact->getMessage().'</li>'.
+                                            '</ul>'.
+                                            '<p>Bonne réception</p>' )
+                                ->setContentType( "text/html" );
+
+                        // On envoie le mail
+                        if ($mailer->send( $message ))
+                            $this->addFlash('success', 'Votre demande de contact a été envoyée !');
+                        else
+                            $this->addFlash('fail', 'Un problème est survenu, recommencez votre demande.');
+
+                        return $this->redirect($this->generateUrl('homepage').'#contact');
+
+                    //
+                }
+            }
         //
 
         /**
@@ -34,7 +92,9 @@ class HomepageController extends AbstractController
          */
             return $this->render(
                 'site/homepage.html.twig' ,
-                array());
+                array(
+                    'form' => $form->createView()
+                ));
         //
     }
 }
---
+++ src/Form/ContactType.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Doctrine\ORM\EntityRepository;
+
+use App\Entity\Contact;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('prenom', TextType::class, [
+                'label' => 'Prénom*',
+                'required' => true,
+                'attr' => ['class' => 'col-lg-12 col-md-12 col-sm-12'],
+                'row_attr' => ['class' => 'col-lg-6 col-md-6 col-sm-12 label-block', 'id' => '']
+            ])
+
+            ->add('nom', TextType::class, [
+                'label' => 'Nom*',
+                'required' => true,
+                'attr' => ['class' => 'col-lg-12 col-md-12 col-sm-12'],
+                'row_attr' => ['class' => 'col-lg-6 col-md-6 col-sm-12 label-block', 'id' => '']
+            ])
+
+            ->add('email', EmailType::class, [
+                'label' => 'E-mail*',
+                'required' => true,
+                'attr' => ['class' => 'col-lg-12 col-md-12 col-sm-12'],
+                'row_attr' => ['class' => 'col-lg-12 col-md-12 col-sm-12 label-block', 'id' => '']
+            ])
+
+            ->add('telephone', TelType::class, [
+                'label' => 'Téléphone',
+                'required' => false,
+                'attr' => ['class' => 'col-lg-12 col-md-12 col-sm-12'],
+                'row_attr' => ['class' => 'col-lg-12 col-md-12 col-sm-12 label-block', 'id' => '']
+            ])
+
+            ->add('message', TextareaType::class, [
+                'label' => 'Message*',
+                'required' => true,
+                'attr' => ['class' => 'col-lg-12 col-md-12 col-sm-12'],
+                'row_attr' => ['class' => 'col-lg-12 col-md-12 col-sm-12 label-block', 'id' => '']
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Contact::class,
+        ]);
+    }
+}
