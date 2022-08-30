57be2663f929a59af72f95f6fa66cef52f746420
marinesl <lancelinmarine@gmail.com>
2022-08-30 17:26:11 +0200

    Page 404

--- config/bundles.php
+++ config/bundles.php
@@ -11,6 +11,5 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
-
-    ConnectHolland\CookieConsentBundle\CHCookieConsentBundle::class => ['all' => true]
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
