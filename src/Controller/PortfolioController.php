a654ce519f699d6ded5da462b98eac4edc6f59ad
Marine Lancelin <lancelinmarine@gmail.com>
2026-03-13 11:12:20 +0100

    ml-8: template portfolio and project + data in json + remove Filterizr

--- src/Controller/PortfolioController.php
+++ src/Controller/PortfolioController.php
@@ -4,44 +4,43 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\File\Exception\FileException;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\String\Slugger\SluggerInterface;
-
-use App\Entity\Projet;
-use App\Entity\Tag;
+use Symfony\Component\Routing\Attribute\Route;
 
 class PortfolioController extends AbstractController
 {
-    /**
-     * Page Portfolio du site internet
-     */
     #[Route('/portfolio', name: 'portfolio')]
     public function index(): Response
     {
-        /**
-         * VUE
-         */
-            return $this->render('site/portfolio.html.twig');
-        //
+        // TODO: faire un service
+        // Path to the JSON file
+        $path = $this->getParameter('kernel.project_dir') . '/public/data/projects.json';
+
+        // Read and decode JSON
+        $projects = json_decode(file_get_contents($path), true);
+
+        return $this->render('site/portfolio.html.twig', [
+            'projects' => $projects,
+        ]);
     }
 
-
-    /**
-     * Page d'un projet dans le portfolio
-     */
     #[Route('/portfolio/{token}', name: 'portfolio_projet')]
-    public function projet(): Response
+    public function getProject(string $token): Response
     {
+        // Path to the JSON file
+        $path = $this->getParameter('kernel.project_dir') . '/public/data/projects.json';
 
+        // Read and decode JSON
+        $projects = json_decode(file_get_contents($path), true);
 
-        /**
-         * VUE
-         */
-            return $this->render(
-                'site/portfolio_projet.html.twig');
-        //
+        // Check if the project exists
+        if (!isset($projects[$token])) {
+            throw $this->createNotFoundException('Projet non trouvé');
+        }
+
+        $project = $projects[$token];
+
+        return $this->render('site/portfolio_projet.html.twig', [
+            'project' => $project,
+        ]);
     }
 }
