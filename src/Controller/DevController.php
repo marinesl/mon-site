fabeded8ac7d6c33f08fca2698c8fe7cbff343a1
Marine Lancelin <lancelinmarine@gmail.com>
2026-05-18 12:46:30 +0200

    ml-3: home page with buttons for my 2 services + create 2 homepages with 2 different routes for the 2 services + some link fixes

---
+++ src/Controller/DevController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\ContactType;
+use App\Service\MailerService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Attribute\Route;
+
+class DevController extends AbstractController
+{
+    public function __construct(
+        private readonly MailerService $mailerService
+    ) {
+    }
+
+    #[Route('/dev', name: 'dev', methods: ['GET', 'POST'])]
+    public function index(Request $request): Response
+    {
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            try {
+                $this->mailerService->sendMailContact($form->getData());
+                $this->addFlash('success', 'Votre demande de contact a été envoyée !');
+            } catch (\Throwable $e) {
+                $this->addFlash('fail', 'Un problème est survenu, recommencez votre demande.' . $e);
+            }
+
+            return $this->redirect($this->generateUrl('dev') . '#contact');
+        }
+
+        return $this->render('dev/homepage.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -2,40 +2,15 @@
 
 namespace App\Controller;
 
-use App\Service\MailerService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
-
-use App\Form\ContactType;
 
 class HomepageController extends AbstractController
 {
-    public function __construct(
-        private readonly MailerService $mailerService
-    ) {
-    }
-
-    #[Route('/', name: 'homepage', methods: ['GET', 'POST'])]
-    public function index(Request $request): Response
+    #[Route('/', name: 'home')]
+    public function index(): Response
     {
-        $form = $this->createForm(ContactType::class);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            try {
-                $this->mailerService->sendMailContact($form->getData());
-                $this->addFlash('success', 'Votre demande de contact a été envoyée !');
-            } catch (\Throwable $e) {
-                $this->addFlash('fail', 'Un problème est survenu, recommencez votre demande.' . $e);
-            }
-
-            return $this->redirect($this->generateUrl('homepage') . '#contact');
-        }
-
-        return $this->render('site/homepage.html.twig', [
-            'form' => $form->createView(),
-        ]);
+        return $this->render('home.html.twig');
     }
 }
