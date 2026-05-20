eceada2e32535222d4e3b9ce0a9ee6208aeb5f9e
Marine Lancelin <lancelinmarine@gmail.com>
2026-05-20 11:21:29 +0200

    ml-3: update content in homepage for dev

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -2,7 +2,6 @@
 
 namespace App\Form;
 
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -11,9 +10,6 @@ use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Doctrine\ORM\EntityRepository;
-
-use App\Entity\Contact;
 
 class ContactType extends AbstractType
 {
